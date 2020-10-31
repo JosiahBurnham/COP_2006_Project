d913d3e0100f966ab3e6144add01fbc5f0cf238c
Josiah Burnham <josiah.burnham1@gmail.com>
2020-10-31 13:22:59 -0400

    Added Comments, IOHelper Class, and futher streamlined what the GradeBook class will be.

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -3,14 +3,27 @@ import java.util.List;
 
 public class GradeBook {
 
+    // The Hashmap that contains all the courses
+    // and all the weights for each of those assignments
     public HashMap<String,HashMap<String,Integer>>  coursesAndWeightedAssignments =
             new HashMap<>();
+    private List<String> courses;
+    private HashMap <String, Integer> assignmentsAndWeights;
 
     public GradeBook(List<String> courses, HashMap<String, Integer> assignmentsAndWeights){
-        for(String course : courses){
-            coursesAndWeightedAssignments.put(course,assignmentsAndWeights);
-        }
 
-        System.out.println(coursesAndWeightedAssignments);
+        this.courses = courses;
+        this.assignmentsAndWeights = assignmentsAndWeights;
+
+        // loops through the list of courses that were passed in
+        // and adds the course and the weights for assignments that go with it
+        for(String course : courses) {
+            coursesAndWeightedAssignments.put(course, assignmentsAndWeights);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return coursesAndWeightedAssignments.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        HashMap<String, Integer> assignmentsAndWeights = new HashMap<>();
-        assignmentsAndWeights.put("Quizzes",225);
-        assignmentsAndWeights.put("Exams",100);
-        assignmentsAndWeights.put("Homework",250);
-        assignmentsAndWeights.put("Project",125);
+
+        // The types of assignments and the weights for each type of assignment
+        // Out of a 700 point scale
+        final HashMap<String, Integer> ASSIGNMENTSANDWEIGHTS = new HashMap<>();
+        ASSIGNMENTSANDWEIGHTS.put("Quizzes",225);
+        ASSIGNMENTSANDWEIGHTS.put("Exams",100);
+        ASSIGNMENTSANDWEIGHTS.put("Homework",250);
+        ASSIGNMENTSANDWEIGHTS.put("Project",125);
+
+        // the List of Courses that are available for a person to input or see grades for.
+        final List<String> COURSES = new ArrayList<>();
+        COURSES.add("COP 2006");
+        COURSES.add("COP 1500");
+        COURSES.add("MAC 2312");
+        COURSES.add("PHY 2048");
+        COURSES.add("PHY 2048L");
+        COURSES.add("ENC 1102");
+        COURSES.add("GLY 1010C");
+
+        GradeBook gradeBook = new GradeBook(COURSES,ASSIGNMENTSANDWEIGHTS);
+
+
 
 
     }
