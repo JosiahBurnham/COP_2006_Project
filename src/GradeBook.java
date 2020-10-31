2cff7e2a0510c057d363a184784bd1812ae04669
Josiah Burnham <josiah.burnham1@gmail.com>
2020-10-31 12:05:43 -0400

    Created Main, foundations for entering Courses and Assignments, and constructor for GradeBook

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,6 +1,16 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class GradeBook {
 
-    public static void main(String[] args) {
-        System.out.println("Initial Commit");
+    public HashMap<String,HashMap<String,Integer>>  coursesAndWeightedAssignments =
+            new HashMap<>();
+
+    public GradeBook(List<String> courses, HashMap<String, Integer> assignmentsAndWeights){
+        for(String course : courses){
+            coursesAndWeightedAssignments.put(course,assignmentsAndWeights);
+        }
+
+        System.out.println(coursesAndWeightedAssignments);
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> assignmentsAndWeights = new HashMap<>();
+        assignmentsAndWeights.put("Quizzes",225);
+        assignmentsAndWeights.put("Exams",100);
+        assignmentsAndWeights.put("Homework",250);
+        assignmentsAndWeights.put("Project",125);
+
+
+    }
+}
